5b69067324462768997f661d167e7c10cb445207
Andrei Munteanu <muntyanua@internet.ru>
2024-06-19 23:27:12 +0300

    1 zadacha

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,25 @@
-﻿// задание 1
-// тут вы напишите свой код
+﻿// Напишите программу, 
+// которая на вход принимает два числа и выводит, какое число большее, 
+// а какое меньшее.
+
+
+int max = 0;
+int min = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
+Console.WriteLine("max = " + max + " и " + "min = " + min);
